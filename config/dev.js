1f154f105925b66c67ad12693211680a9d52fc55
Khang Phan <khang.phan@opswat.com>
2022-08-11 11:26:34 +0700

    init

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
+  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
+  // mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://nodestudent:nodepassword@master-shard-00-00.3e3h4.mongodb.net:27017,master-shard-00-01.3e3h4.mongodb.net:27017,master-shard-00-02.3e3h4.mongodb.net:27017/node-app?ssl=true&replicaSet=atlas-11egwe-shard-0&authSource=admin&retryWrites=true&w=majority',
+  cookieKey: '123123123',
+};
